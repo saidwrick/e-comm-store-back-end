63a8eeac00e7988ab5d3d59f766df675d637bf14
saidwrick <cedric.a.fung@gmail.com>
2024-11-04 14:16:33 -0500

    prepare for gcloud deploy, cors

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ require('dotenv').config()
 const indexRouter = require('./routes/index');
 
 const app = express();
+//cors??
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
